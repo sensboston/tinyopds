bc7b8a925537da96e913a4cfabf26a2254441c9e
Sergey Svinolobov <sensboston@hotmail.com>
2025-09-18 12:24:14 -0400

    small fix for version

--- TinyOPDS/MainForm.cs
+++ TinyOPDS/MainForm.cs
@@ -458,6 +458,7 @@ namespace TinyOPDS
                 databaseFileName.Text = "books.sqlite";
             }
 
+            appVersion.Text = string.Format(Localizer.Text("version {0}.{1} {2}"), Utils.Version.Major, Utils.Version.Minor, Utils.Version.Major == 0 ? " (beta)" : "");
             filterByLanguage.Checked = Settings.Default.FilterBooksByInterfaceLanguage;
             oneInstance.Checked = Settings.Default.OnlyOneInstance;
 
